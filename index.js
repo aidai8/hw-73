af233ae303d3b63079ec5124b2781503d4581577
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2025-03-14 18:17:45 +0600

    First task done

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/', (req, res) => {
+   return res.send('Homepage!');
+});
+
+app.get('/hello', (req, res) => {
+   return res.send('Hello!');
+});
+
+app.get('*', (req, res) => {
+   return res.status(404).send('<h1>Not found</h1>');
+});
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
