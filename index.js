c42df010391f654908b4dc80db693859a2640d00
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2025-03-14 18:46:56 +0600

    Second task done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const {Vigenere} = require('caesar-salad');
 
 const app = express();
 const port = 8000;
+const password = '888';
 
 app.get('/', (req, res) => {
    return res.send('Homepage!');
@@ -9,6 +11,18 @@ app.get('/', (req, res) => {
 
 app.get('/hello', (req, res) => {
    return res.send('Hello!');
+});
+
+app.get('/encode/:text', (req, res) => {
+   const {text} = req.params;
+   const encrypted = Vigenere.Cipher(password).crypt(text);
+   return res.send(encrypted);
+});
+
+app.get('/decode/:text', (req, res) => {
+   const {text} = req.params;
+   const decrypted = Vigenere.Decipher(password).crypt(text);
+   return res.send(decrypted);
 });
 
 app.get('*', (req, res) => {
